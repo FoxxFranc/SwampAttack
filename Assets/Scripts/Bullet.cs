b445b44008f7534a2dd9827ab4cf0ca67d557767
FoxxFranc <74159513+FoxxFranc@users.noreply.github.com>
2021-06-23 18:36:15 +0300

    Init

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private int _dammage;
+    [SerializeField] private float _speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * _speed * Time.deltaTime, Space.World);
+    }
+}
